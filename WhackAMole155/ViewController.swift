10225b8b56966d70a006ee9ac9ae0bce4ede83cc
Brett Berglund <brett.berglund.069@k12.friscoisd.org>
2020-10-15 14:28:01 -0500

    Added Score Label to the top left of the screen

--- WhackAMole155/ViewController.swift
+++ WhackAMole155/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
+var screenWidth = 0
+var screenHeight = 0
+var score = 0
+var scoreLabel = UILabel()
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let screenBounds: CGRect = UIScreen.main.bounds
+        screenWidth = Int(screenBounds.width)
+        screenHeight = Int(screenBounds.height)
+        
+        // Score Label
+        scoreLabel.frame = CGRect(x: 20, y: 60, width: 40, height: 40)
+        scoreLabel.backgroundColor = UIColor.green
+        scoreLabel.text = "\(score)"
+        
+        view.addSubview(scoreLabel)
+        
+        //
+        
+        self.view = view
+        
     }
 
 
